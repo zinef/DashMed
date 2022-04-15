c7b838b6d8775ceae21233ebb45a5dd27d0b5311
FODIL zine-eddine <zineeddine.fodil99@gmail.com>
2022-04-15 22:44:14 +0200

    changes ...

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from DashMed.dataloader import dataloader
+from DashMed.vectorizer import vectorizer
+# import pickle
+# infile = open('corpus','rb')
+# corpus_list = pickle.load(infile)
+# print(corpus_list[0])
+# infile.close()
 
-print(dataloader.articles[21])
+vectorizer.vectorize()
+vectorizer,X = vectorizer.get_params()
+
